1532ef6a89aa8b805f23510b0c0f0dc64667981a
s.degambe@gmail.com <Sebad#21>
2015-10-30 22:02:24 +0100

    nUnit test added

---
+++ BlockControl.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlockControl.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MainViewModel mv = new MainViewModel();
+            mv.CanChanged = true;
+            Assert.IsTrue(mv.CanChanged);
+        }
+    }
+}
